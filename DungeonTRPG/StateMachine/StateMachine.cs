b6a277ed1858f3a7f94f732e9733ec60dba61fbb
Sudo0928 <57037151+Sudo0928@users.noreply.github.com>
2025-02-06 20:55:58 +0900

    StateMachine

---
+++ DungeonTRPG/StateMachine/StateMachine.cs
@@ -0,0 +1,21 @@
+﻿using DungeonTRPG.Entity.Enemy;
+using DungeonTRPG.Entity.Player;
+using DungeonTRPG.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonTRPG.StateMachine
+{
+    internal class StateMachine
+    {
+        private IState currentState;
+        private Stack<IState> preState;
+
+        internal Player Player { get; }
+        internal Enemy Enemy { get; set; }
+        internal int ExploredCount { get; set; }
+    }
+}
