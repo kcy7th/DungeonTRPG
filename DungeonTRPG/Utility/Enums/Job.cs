ff17bef1e47827ab3274a064e98e57b656369ee6
Sudo0928 <57037151+Sudo0928@users.noreply.github.com>
2025-02-07 09:55:09 +0900

    Enum Update

---
+++ DungeonTRPG/Utility/Enums/Job.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonTRPG.Utility.Enums
+{
+    public enum Job
+    {
+        None,
+        Warrior
+    }
+}
