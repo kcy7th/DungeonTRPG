3305ddc00ae10e4da8e8f8814d0d05e301e0c130
Due <kcy7th@naver.com>
2025-02-07 14:22:59 +0900

    feat : Item class, Shop class

--- DungeonTRPG/EntitySystem/Utility/Inventory.cs
+++ DungeonTRPG/EntitySystem/Utility/Inventory.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DungeonTRPG.Utility.Enums;
+using DungeonTRPG.Items;
 
 namespace DungeonTRPG.Entity.Utility
 {
     internal class Inventory
     {
+        private List<Item> items = new List<Item>();  // 아이템 목록
+        private int maxSlots = 10;  // 기본 인벤 슬롯 수
+
+        
+        // 아이템 추가
+        public bool AddItem(Item item)
+        {
+            // 슬롯 남아있을 경우
+            if (items.Count < maxSlots)
+            {
+                items.Add(item);
+                return true;
+            }
+            return false;
+        }
+
+        // 슬롯 확장
+        public void ExpandSlots(int additionalSlots)
+        {
+            maxSlots += additionalSlots;
+        }
+
+        // 보유 아이템 리스트
+        public List<Item> GetItems() => items;
+
+        private Dictionary<EquipSlot, EquipItem> equippedItems = new Dictionary<EquipSlot, EquipItem>();
+
+        // 아이템 장착, 교체
+        public bool EquipItem(EquipSlot slot, EquipItem item)
+        {
+            equippedItems[slot] = item;
+            return true;
+        }
+
+        public EquipItem? GetEquippedItem(EquipSlot slot)
+        {
+            return equippedItems.ContainsKey(slot) ? equippedItems[slot] : null;
+        }
     }
 }
--- DungeonTRPG/ShopSystem/Shop.cs
+++ DungeonTRPG/ShopSystem/Shop.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DungeonTRPG.Entity.Utility;
+using DungeonTRPG.Items;
 
 namespace DungeonTRPG.ShopSystem
 {
     internal class Shop
     {
+        private List<Item> availableItems = new List<Item>();  // 판매 아이템 목록
+
+        // 상점에 아이템 추가
+        public void AddItemToShop(Item item)
+        {
+            availableItems.Add(item);
+        }
+
+        // 아이템 구매 (인벤에 추가, 상점에서 제거)
+        public bool BuyItem(Item item, Inventory playerInventory)
+        {
+            if (playerInventory.AddItem(item))
+            {
+                availableItems.Remove(item);
+                return true;
+            }
+            return false;
+        }
+
+        // 아이템 목록 반환
+        public List<Item> GetItems()
+        {
+            return availableItems;
+        }
     }
 }
