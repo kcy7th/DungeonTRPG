4320ea7c6cdfc56a3b39590b34b10a96d866665f
AAAAAA123222 <kdo03122@gmail.com>
2025-02-09 16:38:33 +0900

    feat: 스킬 세 개 추가 및 틀린 예시 수정
    스킬 세 개를 추가. 다른 스킬은 중복되는 느낌이 있어 범용적으로 활용할 수 있는 것 위주로 작성.
    Rampage: (1+이 스킬로 적을 처치한 횟수)*amount만큼 대상의 체력 감소
    HpDown: amount만큼 체력 감소
    HpDownWithATK: (시전자의 ATK*계수)+amount만큼 대상의 체력 감소 (하지만, HpDown이 충분히 수행할 수 있는 기능이라 큰 의미는 없음)
    
    그리고, HpRecovery의 잘못된 예시 정정

---
+++ DungeonTRPG/EntitySystem/ActiveEffect/HpDown.cs
@@ -0,0 +1,40 @@
+﻿using DungeonTRPG.Entity;
+using DungeonTRPG.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonTRPG.EntitySystem.ActiveEffect
+{
+    internal class HpDown: IEffect
+    {
+        int amount;
+        
+        public HpDown(int amount)
+        {
+            this.amount = amount;
+        }
+
+        //Hp가 amount만큼 줄어드는 효과 클래스
+        //amount를 방어력만큼 설정해서 caster의 방어력만큼 target의 체력을 깎는 것도 생각해볼 수 있을 것 같습니다
+
+        public void UseEffect(Character caster, Character target)
+        {
+            //if (target.stat.Hp-amount<0) //타겟의 체력과 amount의 차가 0일 경우
+            //{
+            //    //체력을 0으로 조정
+            //    target.stat.Hp = 0;
+            //    //이후 값 설정하거나 호출 (isDead, isAlive, battleEnd() 등...)
+            //}
+            //else
+            //{
+            //    target.stat.Hp -= amount;
+            //}
+
+            //// 만약 DamageHp같은 게 있다면 위 식 대신 이걸 쓰기
+            //target.Stat.DamageHp(amount);
+        }
+    }
+}
--- DungeonTRPG/EntitySystem/ActiveEffect/HpRecovery.cs
+++ DungeonTRPG/EntitySystem/ActiveEffect/HpRecovery.cs
@@ -20,6 +20,7 @@ namespace DungeonTRPG.EntitySystem.ActiveEffect
 
         //character 하나만을 갖고 작동하는 기능이지만, 상속된 기능이라 더미 캐릭터 하나를 추가해야만 함
         //character 자리에 enemy를 넣고 amount 자리에 -(player.Stat.Atk*1.25)같은 값을 넣어서 회복 대신 플레이어의 공격력에 비례한 강한 피해를 가하는 공격 스킬로 재사용 가능
+        //생각해보니 쓸 수가 없네요. 조건문때문에 데미지+현재 체력이 최대 체력보다 높으면 최대 체력으로 세팅을 해버려서
 
         public void UseEffect(Character character, Character placeholder)
         {
