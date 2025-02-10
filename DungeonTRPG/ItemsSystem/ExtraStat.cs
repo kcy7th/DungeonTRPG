0f403a5bce79ea41300a6b66330ff620087c00e3
Sudo0928 <57037151+Sudo0928@users.noreply.github.com>
2025-02-10 10:26:56 +0900

    ExtraStat add

---
+++ DungeonTRPG/ItemsSystem/ExtraStat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonTRPG.ItemsSystem
+{
+    internal class ExtraStat
+    {
+        public int Hp { get; }
+        public int Mp { get; }
+        public int Atk { get; }
+        public int Def { get; }
+    }
+}
