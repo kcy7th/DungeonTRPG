ea0ce46ba47d9de71c2e641bbe9605ec33b37e24
AAAAAA123222 <kdo03122@gmail.com>
2025-02-10 17:46:18 +0900

    fix: 중복되는 스킬 ID 변경

--- DungeonTRPG/Manager/Data/SkillDB.cs
+++ DungeonTRPG/Manager/Data/SkillDB.cs
@@ -16,7 +16,7 @@ namespace DungeonTRPG.Manager.Data
             { 9002, new Skill("광란", "피해를 10 입히며, 이 스킬로 대상을 처치할때마다 스킬의 데미지가 20% 오른다.", new List<IEffect>(){new Rampage(10) } ) },
             { 9003, new Skill("무모한 돌격", "스스로에게 피해를 2 입히고, 대상에게 15+공격력의 100%만큼의 데미지를 입힌다.", new List<IEffect>(){new HpRecovery(15) } ) },
             { 9004, new Skill("어지러운 일격", "공격력의 75%만큼의 피해를 입히며, 공격력에 비례해 일정 확률로 기절을 부여한다.", new List<IEffect>(){new HpRecovery(10) } ) },
-            { 9004, new Skill("고함치기", "대상의 방어력을 5, 공격력을 3 감소시킨다. 상대방보다 레벨이 낮다면 실패할 수도 있다.", new List<IEffect>(){new DefDown(5), new AtkDown(3) } ) },
+            { 9005, new Skill("고함치기", "대상의 방어력을 5, 공격력을 3 감소시킨다. 상대방보다 레벨이 낮다면 실패할 수도 있다.", new List<IEffect>(){new DefDown(5), new AtkDown(3) } ) },
         };
 
         public Skill GetByKey(int key)
