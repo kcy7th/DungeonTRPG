2790862b73afa2b0d8a1cf542c6251598cabd42b
AAAAAA123222 <kdo03122@gmail.com>
2025-02-10 17:43:12 +0900

    Feat: 공격력, 방어력 감소 효과, 공격력과 방어력을 감소시키는 스킬 추가
    공격력과 방어력 감소에 턴 제한이 없어 만약 플레이어를 대상으로 사용하게 된다면 문제가 될 것 같습니다.

---
+++ DungeonTRPG/EntitySystem/ActiveEffect/DefDown.cs
@@ -0,0 +1,44 @@
+﻿using DungeonTRPG.Entity;
+using DungeonTRPG.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonTRPG.EntitySystem.ActiveEffect
+{
+    internal class DefDown: IEffect
+    {
+
+        private int amount;
+
+        public DefDown(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public void UseEffect(Character player, Character enemy)
+        {
+            Random random= new Random();
+            int roll = (int)(random.Next(1, 10) +player.Stat.Atk*0.1);
+
+            if (player.Stat.Lv<enemy.Stat.Lv) //플레이어 레벨이 적보다 낮다면
+            {
+                if (roll > 5) 
+                {
+                    enemy.Stat.SetDef(enemy.Stat.Def- amount); //적의 방어력 amount만큼 감소
+                    //가능하다면 공격력 감소 State를 추가해서 일정 턴동안만 유지될 수 있게 하면 좋을 것 같습니다.
+                }
+                //else
+                //{
+                //    //필요하다면,실패 알림 출력하기
+                //}
+            }
+            else
+            {
+                enemy.Stat.SetDef(enemy.Stat.Def - amount); //적의 방어력 amount만큼 감소
+            }
+        }
+    }
+}
--- DungeonTRPG/Manager/Data/SkillDB.cs
+++ DungeonTRPG/Manager/Data/SkillDB.cs
@@ -19,11 +19,12 @@ namespace DungeonTRPG.Manager.Data
         [JsonProperty]
         public Dictionary<int, Skill> Items { get; } = new Dictionary<int, Skill>()
         {
-            { 9000, new Skill("회복", "체력을 10 회복한다.", UseableIn.OnlyCombat, new List<IEffect>(){new HpRecovery(10) } ) },
-            { 9001, new Skill("강한 타격", "피해를 10 + 공격력의 50%만큼 입힌다", UseableIn.OnlyCombat, new List<IEffect>(){new HpDownWithATK(10,0.5f) } ) },
-            { 9002, new Skill("광란", "피해를 10 입히며, 처치할때마다 데미지가 20% 오른다.", UseableIn.OnlyCombat, new List<IEffect>(){new Rampage(10) } ) },
-            { 9003, new Skill("무모한 돌격", "스스로에게 피해를 2 입히고, 대상에게 15+공격력의 100%만큼의 데미지를 입힌다.", UseableIn.OnlyCombat, new List<IEffect>(){new HpRecovery(15) } ) },
-            { 9004, new Skill("어지러운 일격", $"공격력의 75%만큼의 피해를 입히며, 공격력에 비례해 일정 확률로 기절을 부여한다.", UseableIn.OnlyCombat, new List<IEffect>(){new HpRecovery(10) } ) },
+            { 9000, new Skill("회복", "체력을 10 회복한다.", new List<IEffect>(){new HpRecovery(10) } ) },
+            { 9001, new Skill("강한 타격", "피해를 10 + 공격력의 50%만큼 입힌다", new List<IEffect>(){new HpDownWithATK(10,0.5f) } ) },
+            { 9002, new Skill("광란", "피해를 10 입히며, 이 스킬로 대상을 처치할때마다 스킬의 데미지가 20% 오른다.", new List<IEffect>(){new Rampage(10) } ) },
+            { 9003, new Skill("무모한 돌격", "스스로에게 피해를 2 입히고, 대상에게 15+공격력의 100%만큼의 데미지를 입힌다.", new List<IEffect>(){new HpRecovery(15) } ) },
+            { 9004, new Skill("어지러운 일격", "공격력의 75%만큼의 피해를 입히며, 공격력에 비례해 일정 확률로 기절을 부여한다.", new List<IEffect>(){new HpRecovery(10) } ) },
+            { 9004, new Skill("고함치기", "대상의 방어력을 5, 공격력을 3 감소시킨다. 상대방보다 레벨이 낮다면 실패할 수도 있다.", new List<IEffect>(){new DefDown(5), new AtkDown(3) } ) },
         };
 
         public Skill GetByKey(int key)
