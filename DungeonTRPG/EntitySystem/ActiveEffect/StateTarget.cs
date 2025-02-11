3efed7ea975ae68fcc9c523afdf7b7b39157a28f
AAAAAA123222 <kdo03122@gmail.com>
2025-02-11 16:48:24 +0900

    feat: 상태이상 효과 개선, 스킬 조정
    Stun 효과를 삭제하고, StateSelf와 StateTarget으로 분리한 뒤 매개변수를 받아 호출하는 방식으로 다시 작성했습니다.

---
+++ DungeonTRPG/EntitySystem/ActiveEffect/StateTarget.cs
@@ -0,0 +1,40 @@
+﻿using DungeonTRPG.Entity;
+using DungeonTRPG.Interface;
+using DungeonTRPG.Utility.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonTRPG.EntitySystem.ActiveEffect
+{
+    internal class StateTarget:IEffect
+    {
+        int probability;
+        State state;
+        public StateTarget(int probability, State state)
+        {
+            this.probability = probability;
+            this.state = state;
+        }
+
+
+        //대상을 특정 확률(roll<probability)로 특정 상태이상(SetState(state))에 걸리게 하는 효과
+        public void UseEffect(Character player, Character enemy)
+        {
+            Random chance = new Random();
+            int roll = (int)(chance.Next(1, 101)); //1에서 100까지의 수를 생성
+
+            if (roll < probability) // 생성된 난수가 확률보다 낮다면 성공
+            {
+                enemy.CharacterState.SetState(state); //enemy의 상태를 효과에 지정된 매개변수로 설정
+                //필요하다면, 성공 메세지 호출하기
+            }
+            else
+            {
+                //필요하다면, 실패 메세지 호출하기
+            }
+        }
+    }
+}
