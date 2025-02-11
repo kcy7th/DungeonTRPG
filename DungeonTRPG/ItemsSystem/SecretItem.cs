a8d25881adbc41081524da51aa3470e07ef217bd
Due <kcy7th@naver.com>
2025-02-11 16:29:53 +0900

    feat : SecretItem 추가

---
+++ DungeonTRPG/ItemsSystem/SecretItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DungeonTRPG.Items;
+using DungeonTRPG.Utility.Enums;
+
+namespace ungeonTRPG.Items
+{
+    internal class SecretItem : Item
+    {
+        public string SpecialEffect { get; }
+
+        public SecretItem(string name, string description, List<Job> allowedJobs, string specialEffect)
+            : base(name, description, allowedJobs)
+        {
+            SpecialEffect = specialEffect;
+        }
+
+        public override Item Clone()
+        {
+            return new SecretItem(name, description, new List<Job>(AllowedJobs), SpecialEffect);
+        }
+
+        public override int CompareTo(Item? other)
+        {
+            if (other is EquipItem) return 0;
+            else return 1;
+        }
+    }
+
+}
